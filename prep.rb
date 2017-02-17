a397f80103332d82c39662b79dfadbe62a7a2d32
Harald Sitter <sitter@kde.org>
2017-02-17 14:38:44 +0100

    setup repo

--- prep.rb
+++ prep.rb
@@ -2,5 +2,9 @@
 
 require '/var/lib/jenkins/ci-tooling/nci/setup_apt'
 
+ENV['TYPE'] = 'release'
+
+NCI.setup_repo!
+
 system('apt update') || raise
 system('apt install -y snapcraft git zsync') || raise
