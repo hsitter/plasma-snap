abc0546a65567f35982e72d61839a2b1e40a28f8
Harald Sitter <sitter@kde.org>
2017-02-17 14:30:58 +0100

    add a crafter script

--- prep.rb
+++ prep.rb
@@ -3,4 +3,4 @@
 require '/var/lib/jenkins/ci-tooling/nci/setup_apt'
 
 system('apt update') || raise
-system('apt install -y snapcraft') || raise
+system('apt install -y snapcraft git zsync') || raise
